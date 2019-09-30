d5bcb7ecd2f60f615f3377d82cae86680ecdffeb
mauget <mauget@gmail.com>
2019-09-30 11:38:40 -0400

    Renamed decrement

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,14 +2,14 @@ import React, {useState, useContext} from "react";
 import {AppContext} from '../App';
 
 export default function Counter() {
-    const [count, setCount] = useState(2000);
+    const [count, setCount] = useState(0);
     const date = useContext(AppContext);
 
     function increment() {
         setCount(count + 1);
     }
 
-    function deccrement() {
+    function decrement() {
         setCount(count - 1);
     }
 
@@ -25,7 +25,7 @@ export default function Counter() {
                 <button className="btn-small" onClick={increment}>Increment value</button>
             </p>
             <p class="flow-text">
-                <button className="btn-small" onClick={deccrement}>Decrement value</button>
+                <button className="btn-small" onClick={decrement}>Decrement value</button>
             </p>
             <p class="flow-text">
                 <button className="btn-small" onClick={reset} disabled={count === 0}>Reset value</button>
--- src/components/Page1.js
+++ src/components/Page1.js
@@ -5,7 +5,7 @@ export default function Page1() {
     return (
         <div className="container">
             <h1>Materialize, state hook, and context</h1>
-            <div className="card-panel hoverable">
+            <div className={"card-panel hoverable"}>
                 <p class="flow-text">To be or not to be ...</p>
                 <Counter/>
             </div>
